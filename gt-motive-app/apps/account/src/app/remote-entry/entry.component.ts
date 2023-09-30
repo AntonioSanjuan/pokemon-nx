a661a122847bcbb9194bec96021c6b8ca893b39d
Tony Sanjuan <tonysanj13@gmail.com>
2023-09-30 15:55:41 +0200

    fix: fixed

--- gt-motive-app/apps/account/src/app/remote-entry/entry.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, inject } from '@angular/core';
-
-@Component({
-  selector: 'gt-motive-app-account-entry',
-  template: ``,
-})
-
-export class RemoteEntryComponent {
-}
--- gt-motive-app/apps/shell/module-federation.config.js
+++ gt-motive-app/apps/shell/module-federation.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   name: 'shell',
-  remotes: ['account'],
+  remotes: [],
 };
