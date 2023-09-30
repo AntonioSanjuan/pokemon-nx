89dcaf9e20862ddf1a1bd7e48e25239a8aafacec
Tony Sanjuan <tonysanj13@gmail.com>
2023-09-30 02:15:07 +0200

    thats it

--- gt-motive-app/apps/account/src/app/login/login.component.ts
+++ gt-motive-app/apps/account/src/app/login/login.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { AuthService } from '@gt-motive-app/libs/services/auth';
 
 @Component({
   selector: 'gt-motive-app-login',
   templateUrl: './login.component.html',
+  imports: [ CommonModule ],
   styleUrls: ['./login.component.scss'],
   standalone: true
 })
