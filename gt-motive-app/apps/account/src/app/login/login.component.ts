9996cc34b487284eeae6e24ef32e3ddd2e7f41f4
Tony Sanjuan <tonysanj13@gmail.com>
2023-10-03 14:17:57 +0200

    feature: using LetDirective

--- gt-motive-app/apps/account/src/app/login/login.component.ts
+++ gt-motive-app/apps/account/src/app/login/login.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { LetDirective, LetModule } from '@ngrx/component';
 import { AuthService } from '@gt-motive-app/libs/services/auth';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'gt-motive-app-login',
   templateUrl: './login.component.html',
-  imports: [ CommonModule ],
+  imports: [ 
+    CommonModule,
+    LetDirective 
+  ],
   styleUrls: ['./login.component.scss'],
   standalone: true
 })
 export class LoginComponent {
   public authService = inject(AuthService)
-
+  public store = inject(Store)
 }
--- gt-motive-app/apps/account/src/app/login/store/login.selectors.ts
+++ gt-motive-app/apps/account/src/app/login/store/login.selectors.ts
@@ -1,1 +1,4 @@
+import { createSelector } from "@ngrx/store";
+import { selectAccountState } from "../../+state/account.selectors";
 
+export const selectLoginState = createSelector(selectAccountState, state => state.login)
