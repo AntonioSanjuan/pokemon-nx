a69465f068d968a754802d9302ef9e7ae4f93083
Tony desktop <tonysanj13@gmail.com>
2023-10-28 14:33:41 +0200

    fix: layout

--- gt-motive-app/apps/account/src/app/app.component.ts
+++ gt-motive-app/apps/account/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'gt-motive-app-account',
   template: '<router-outlet></router-outlet>',
-  // styles: [':host { height: 100%}'],
+  styles: [':host { height: 100%; }'],
   imports: [RouterOutlet],
+  encapsulation: ViewEncapsulation.ShadowDom,
   standalone: true
 })
 export class AppComponent {}
