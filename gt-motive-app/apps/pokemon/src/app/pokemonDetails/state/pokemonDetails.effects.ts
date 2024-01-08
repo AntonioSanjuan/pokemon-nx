4b6c4856ce76dd179851c28a1513046e4a8cf7f3
Tony desktop <tonysanj13@gmail.com>
2024-01-08 14:45:17 +0100

    feature: if getPokemonByName returns no pokemon, should redirect to list & show error

--- gt-motive-app/apps/pokemon/src/app/pokemonDetails/state/pokemonDetails.effects.ts
+++ gt-motive-app/apps/pokemon/src/app/pokemonDetails/state/pokemonDetails.effects.ts
@@ -1,20 +1,25 @@
 import { Injectable, inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { getPokemonByNameRequest, getPokemonByNameRequestError, getPokemonByNameRequestSuccess } from './pokemonDetails.actions';
-import { map, catchError, of, mergeMap } from 'rxjs'
+import { map, catchError, of, mergeMap, tap } from 'rxjs'
 import { PokemonDetailsService } from './pokemonDetails.service';
-import { PokemonResponseDto } from '@gt-motive-app/libs/models';
+import { AppRoutes, PokemonResponseDto } from '@gt-motive-app/libs/models';
+import { Router } from '@angular/router';
 @Injectable()
 export class PokemonDetailsEffects {
     private pokemonDetailsService: PokemonDetailsService = inject(PokemonDetailsService)
     private actions$: Actions = inject(Actions);
+    private router: Router = inject(Router)
 
     getPokemonByNameRequest$ = createEffect(() => this.actions$.pipe(
         ofType(getPokemonByNameRequest),
         mergeMap(({ pokemonName }) => 
             this.pokemonDetailsService.getPokemonByName(pokemonName).pipe(
                 map((pokemon: PokemonResponseDto) => getPokemonByNameRequestSuccess({pokemon})),
-                catchError(_ => of(getPokemonByNameRequestError()))
+                catchError(_ => {
+                    this.router.navigate([AppRoutes.PokemonList])
+                    return of(getPokemonByNameRequestError())}
+                )
             )
         )
     ))
