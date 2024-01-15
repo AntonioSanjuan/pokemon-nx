e7b2125a3de8130f0a03f6c54f6ea5b3593dd99b
Tony Sanjuan <tonysanj13@gmail.com>
2024-01-15 17:18:49 +0100

    feature: added stat details

--- gt-motive-app/apps/shell/src/app/layout/common/profile-nav/profile-nav-bar.component.ts
+++ gt-motive-app/apps/shell/src/app/layout/common/profile-nav/profile-nav-bar.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '@gt-motive-app/libs/services/auth';
 import { getIsUserLogged } from '@gt-motive-app/store';
 import { UiModule } from '@gt-motive-app/ui';
